6ecff0c2e97fe1fd20822b1f9f3c787723e7cc79
zhouxishun <1050494995@qq.com>
2020-09-24 09:57:49 +0800

    cli初始化命令

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,51 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+const program = require('commander')
 
 const pkg = require('../package.json')
+const files = require('../')
+
+const commandMap = {
+  init: {
+    command: 'init <name>',
+    alias: 'i',
+    description: '初始化项目文件',
+    examples: ['zxs init <projectName>', 'zxs iiii']
+  }
+}
+
+const commandKeys = Reflect.ownKeys(commandMap)
+
+// 生成命令
+commandKeys.forEach((name) => {
+  const { command, alias, description } = commandMap[name]
+  program
+    .command(command)
+    .alias(alias)
+    .description(description)
+    .action((...args) => {
+      files(name, ...args)
+    })
+})
+
+// 添加 --help 输出的内容
+program.on('--help', () => {
+  console.log('Examples: ')
+  commandKeys.forEach((coms) => {
+    const { examples } = commandMap[coms]
+    const examplesMsg = examples.join('\n').trim()
+    console.log('  ' + examplesMsg)
+  })
+})
 
 program.version(pkg.version)
   .usage('<command> [options]')
 
-program.command('init <name>')
-  .description('初始化项目文件')
-  .action((name, opt) => {
-    require('../lib/init')(name, opt)
-    // console.log(name, argv)
-    // inquirer.prompt([
-    //   {
-    //     type: 'input',
-    //     name: 'description',
-    //     message: '请输入项目的描述信息',
-    //     default: ''
-    //   },
-    //   {
-
-    //   }
-    // ])
-  })
+// program.command('init <name>')
+//   .alias('i')
+//   .description('初始化项目文件')
+//   .action((name, opt) => {
+//     require('../lib/init')(name, opt)
+//   })
 
 program.parse(process.argv) // 解析变量
